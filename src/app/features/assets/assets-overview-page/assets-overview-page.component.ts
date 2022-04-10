e717e6d793131dc3a98b06c4039435ca68721691
Marius Jäger <jaeger-marius@web.de>
2022-04-10 15:34:37 +0200

    impl stock overview

--- src/app/features/assets/assets-overview-page/assets-overview-page.component.ts
+++ src/app/features/assets/assets-overview-page/assets-overview-page.component.ts
@@ -1,21 +1,61 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { StockModel } from 'src/app/models/stock-model';
+import { TaxRelevantDataModel } from 'src/app/models/tax-relevant-data-model';
 
 @Component({
   selector: 'app-assets-overview-page',
   templateUrl: './assets-overview-page.component.html',
-  styleUrls: ['./assets-overview-page.component.css']
+  styleUrls: ['./assets-overview-page.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class AssetsOverviewPageComponent implements OnInit {
-  saleData = [
-    { name: "Mobiles", value: 105000 },
-    { name: "Laptop", value: 55000 },
-    { name: "AC", value: 15000 },
-    { name: "Headset", value: 150000 },
-    { name: "Fridge", value: 20000 }
-  ];
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AssetsOverviewPageComponent {
+  dataSource = stockData;
+  columnsToDisplay = ['name', 'amount', 'value', 'performanceDay', 'performanceAllTime'];
+  expandedElement: TaxRelevantDataModel | null;
 }
+
+const stockData: StockModel[] = [
+  {
+  name: 'Amazon',
+  isbn: 'string',
+  symbol: 'TSLA',
+  amount: 2,
+  value: 3000,
+  performanceDay: 3,
+  performanceAllTime: 200,
+},
+{
+  name: 'Microsoft',
+  isbn: 'string',
+  symbol: 'TSLA',
+  amount: 10,
+  value: 250,
+  performanceDay: -3,
+  performanceAllTime: 200,
+},
+{
+  name: 'SAP',
+  isbn: 'string',
+  symbol: 'TSLA',
+  amount: 10,
+  value: 100,
+  performanceDay: 3,
+  performanceAllTime: 50,
+},
+{
+  name: 'Tesla',
+  isbn: 'string',
+  symbol: 'TSLA',
+  amount: 10,
+  value: 1000,
+  performanceDay: 3,
+  performanceAllTime: 200,
+},
+]
---
+++ src/app/models/stock-model.ts
@@ -0,0 +1,9 @@
+export interface StockModel {
+    name: string,
+    isbn: string,
+    symbol: string,
+    amount: number,
+    value: number,
+    performanceDay: number,
+    performanceAllTime: number,
+}
